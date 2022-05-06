d42d4ca54bc7e22c9e1098b060a6f0614b97a337
Alvaro Rago <alvarorc.sistemas@gmail.com>
2022-05-05 23:09:14 -0300

    feat(shop): add remote application

--- apps/shell/src/app/app.tsx
+++ apps/shell/src/app/app.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import NxWelcome from './nx-welcome';
 import { Link, Route, Routes } from 'react-router-dom';
 
+const Shop = React.lazy(() => import('shop/Module'));
+
 export function App() {
   return (
     <React.Suspense fallback={null}>
@@ -9,9 +11,13 @@ export function App() {
         <li>
           <Link to="/">Home</Link>
         </li>
+        <li>
+          <Link to="/shop">Shop</Link>
+        </li>
       </ul>
       <Routes>
         <Route path="/" element={<NxWelcome title="shell" />} />
+        <Route path="/shop" element={<Shop />} />
       </Routes>
     </React.Suspense>
   );
---
+++ apps/shop/src/app/app.tsx
@@ -0,0 +1,10 @@
+export function App() {
+  return (
+    <>
+      <h1>Hello</h1>
+      <p>test HMR</p>
+    </>
+  );
+}
+
+export default App;
